44b7a56455b52e18a637b81cff6a9841affca3a9
abiodun6876 <abiodunmosesakinpelu@gmail.com>
2025-12-29 14:14:32 +0100

    styleupdate

--- src/pages/AttendancePage.tsx
+++ src/pages/AttendancePage.tsx
@@ -35,7 +35,7 @@ import FaceCamera from '../components/FaceCamera';
 import { supabase } from '../lib/supabase';
 import faceRecognition from '../utils/faceRecognition';
 import dayjs from 'dayjs';
-import './AttendancePage.css';
+import '../AttendancePage.css';
 
 const { Title, Text } = Typography;
 const { useBreakpoint } = Grid;
