3a06d7c6ad1527069eeb7da46de3481220a58e5e
abiodun6876 <abiodunmosesakinpelu@gmail.com>
2026-02-14 15:37:51 +0100

    Final cleanup: Remove mobile directory and add cleanup documentation

--- mobile/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add support for .bin files (TensorFlow.js model weights)
-config.resolver.assetExts.push('bin');
-
-module.exports = config;
